c8b248a2b2ee85334816258b2fda94b101c19968
Vinícius Santos <vinicius.bodruch@aluno.faculdadeimpacta.com.br>
2022-02-03 21:17:30 -0300

    Added Seeding Service and Sellers Controller

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Data
 {
@@ -14,8 +9,8 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<SalesWebMvc.Models.Seller> Seller { get; set; }
+        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecord { get; set; }
     }
 }
